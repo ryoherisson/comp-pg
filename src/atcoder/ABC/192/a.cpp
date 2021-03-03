53052a8a6c44b76de73cfc698262ab1189bce030
ryoherisson <ryhizw@gmail.com>
2021-03-03 10:40:06 +0900

    192

---
+++ src/atcoder/ABC/192/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define REP(i, m, n) for(int (i) = (m); (i) < (n); ++i)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+using Graph = vector< vector<int> >;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+typedef long long ll;
+typedef pair<ll, ll> P;
+const int INF = 1e9+7;
+const ll LINF = 1LL<<60;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    int x;
+    cin >> x;
+
+    cout << 100 - x % 100 << endl;
+
+
+    return 0;
+}
