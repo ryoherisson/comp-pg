1acd24b7f269174d9d9a1f49a6562142ad496b9a
ryoherisson <ryhizw@gmail.com>
2021-03-27 23:58:24 +0900

    189

---
+++ src/atcoder/ABC/189/a.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define REP(i, m, n) for (int(i) = (m); (i) < (n); ++i)
+#define rep(i, n) REP(i, 0, n)
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+using Graph = vector<vector<int>>;
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+typedef long long ll;
+typedef pair<ll, ll> P;
+const int INF = 1e9 + 7;
+const ll LINF = 1LL << 60;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    string c;
+    cin >> c;
+
+    if (c[0] == c[1] && c[1] == c[2])
+        cout << "Won" << endl;
+    else
+        cout << "Lost" << endl;
+
+    return 0;
+}
