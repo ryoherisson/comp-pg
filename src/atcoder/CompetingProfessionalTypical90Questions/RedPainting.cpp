67309dd9b57b43044ce8f63f237f9d8345014600
ryoherisson <ryhizw@gmail.com>
2022-02-24 09:02:21 +0900

    fix redpainting

--- src/atcoder/CompetingProfessionalTypical90Questions/RedPainting.cpp
+++ src/atcoder/CompetingProfessionalTypical90Questions/RedPainting.cpp
@@ -42,51 +42,49 @@ bool used[2009][2009];
 void query1(int px, int py) {
 	int dx[4] = { -1, 0, 1, 0 };
 	int dy[4] = { 0, 1, 0, -1 };
-    for (int i = 0; i < 4; i++) {
-        int sx = px + dx[i], sy = py + dy[i];
-        if (used[sx][sy] == false) continue;
-        int hash1 = (px - 1) * W + (py - 1);
-        int hash2 = (sx - 1) * W + (sy - 1);
-        UF.unite(hash1, hash2);
-    }
-    used[px][py] = true;
+	for (int i = 0; i < 4; i++) {
+		int sx = px + dx[i], sy = py + dy[i];
+		if (used[sx][sy] == false) continue;
+		int hash1 = (px - 1) * W + (py - 1);
+		int hash2 = (sx - 1) * W + (sy - 1);
+		UF.unite(hash1, hash2);
+	}
+	used[px][py] = true;
 }
 
 bool query2(int px, int py, int qx, int qy) {
-    if (used[px][py] == false || used[qx][qy] == false) {
-        return false;
-    }
+	if (used[px][py] == false || used[qx][qy] == false) {
+		return false;
+	}
 
-    int hash1 = (px - 1) * W + (py - 1);
-    int hash2 = (qx - 1) * W + (qy - 1);
-    if (UF.same(hash1, hash2) == true) return true;
-    return false;
+	int hash1 = (px - 1) * W + (py - 1);
+	int hash2 = (qx - 1) * W + (qy - 1);
+	if (UF.same(hash1, hash2) == true) return true;
+	return false;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	// Step #1. 入力
+	cin >> H >> W >> Q;
 
-    cin >> H >> W >> Q;
+	// Step #2. Union Find の初期化
+	UF.init(H * W);
 
-    UF.init(H * W);
-
-    for (int i = 0; i <= Q; i++) {
-        int ty; cin >> ty;
-        if (ty == 1) {
-            int x, y;
-            cin >> x >> y;
-            query1(x, y);
-        }
-        if (ty == 2) {
-            int xa, xb, ya, yb;
-            cin >> xa >> xb >> ya >> yb;
-            bool Answer = query2(xa, ya, xb, yb);
-            if (Answer == true) cout << "Yes" << endl;
-            else cout << "No" << endl;
-        }
-    }
-
-    return 0;
+	// Step #3. クエリ処理
+	for (int i = 1; i <= Q; i++) {
+		int ty; cin >> ty;
+		if (ty == 1) {
+			int x, y;
+			cin >> x >> y;
+			query1(x, y);
+		}
+		if (ty == 2) {
+			int xa, xb, ya, yb;
+			cin >> xa >> ya >> xb >> yb;
+			bool Answer = query2(xa, ya, xb, yb);
+			if (Answer == true) cout << "Yes" << endl;
+			else cout << "No" << endl;
+		}
+	}
+	return 0;
 }
